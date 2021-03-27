3476c08a16475777cf1a74eb9f7ff514e59fb959
sebastabera <sebas03700@hotmail.com>
2021-03-27 15:41:14 -0500

    clase logica

---
+++ empleado.php
@@ -0,0 +1,34 @@
+<?php
+
+    class Empleado{
+
+        private $cedula;
+        private $nombre;
+        private $sueldo;
+        private $dias;
+        private $hed;
+        private $hen;
+        private $hedd;
+        private $hedn;
+        private $basico;
+        private $aux_trans;
+        private $total_extra;
+        private $total_devengado;
+        private $salud;
+        private $pension;
+        private $arl;
+        private $icbf;
+        private $fondo_solidario;
+        private $retefuente;
+        private $total_parafiscales;
+        private $prima;
+        private $vacaciones;
+        private $cesantias;
+        private $int_cesantias;
+        private $total_prestaciones;
+        
+        public function_construct()
+
+    }
+
+?>

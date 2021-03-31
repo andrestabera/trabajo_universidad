88745b776c4e74441394db39cb36649f141fc131
Luis Sebastian Tabera Quintero <lstabera@personalsoft.com.co>
2021-03-30 20:36:27 -0500

    ver mas

---
+++ ver.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <title>Document</title>
+</head>
+<body>
+    
+    <div class="container pt-6">
+        <h3>Empleado</h3>
+        <div class="row">
+            <div class="col-sm-2">Nombre</div>
+            <div class="col-sm-10">$empleado["nombre"]</div>
+            <div class="col-sm-2">Cedula</div>
+            <div class="col-sm-10"></div>
+            <div class="col-sm-2"></div>
+            <div class="col-sm-10"></div>
+            <div class="col-sm-2"></div>
+            <div class="col-sm-10"></div>
+            <div class="col-sm-2"></div>
+            <div class="col-sm-10"></div>
+            <div class="col-sm-2"></div>
+            <div class="col-sm-10"></div>
+            <div class="col-sm-2"></div>
+            <div class="col-sm-10"></div>
+            <div class="col-sm-2"></div>
+            <div class="col-sm-10"></div>
+            <div class="col-sm-2"></div>
+            <div class="col-sm-10"></div>
+            <div class="col-sm-2"></div>
+            <div class="col-sm-10"></div>
+            <div class="col-sm-2"></div>
+            <div class="col-sm-10"></div>
+            <div class="col-sm-2"></div>
+            <div class="col-sm-10"></div>
+            <div class="col-sm-2"></div>
+            <div class="col-sm-10"></div>
+            <div class="col-sm-2"></div>
+            <div class="col-sm-10"></div>
+            <div class="col-sm-2"></div>
+            <div class="col-sm-10"></div>
+        </div>
+    </div>
+
+</body>
+</html>
